b861888e548397a8281a35793756568aa327dd72
Vladimir <vladkodoktorov@gmail.com>
2022-05-27 16:47:21 +0100

    Add project files.

---
+++ LimitRequests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RateLimitingExample
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class LimitRequests : Attribute
+    {
+        public int TimeWindow { get; set; } // seconds
+        public int MaxRequests { get; set; }  // requests
+    }
+}
